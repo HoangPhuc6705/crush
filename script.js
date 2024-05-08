6d35e1820001df7831623d247312c78237339aef
HoangPhuc6705 <tonguyenhoangphuc6705@gmail.com>
2024-05-08 20:50:03 +0700

    push

---
+++ script.js
@@ -0,0 +1,23 @@
+const button = document.getElementById('confirm');
+const result = document.getElementById('result');
+
+button.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  const name = document.getElementById('yourname__name').value;
+  const crushname = document.getElementById('yourcrushname__name').value;
+
+  const ratio_of_successfull = Math.round(Math.random() * 101);
+
+  result.innerHTML = `Tỉ lệ thành công của bạn và crush là: ${ratio_of_successfull}%`
+
+  const data = {
+    name: name,
+    crushname: crushname,
+    ratio_of_successfull: ratio_of_successfull
+  }
+
+  axios.post('http://localhost:3000/crushs', data).then(() => {
+    alert('Dữ liệu đã được lưu thành công')
+  }).then(() => console.log('Send')).catch((err) => console.error(err));
+})
